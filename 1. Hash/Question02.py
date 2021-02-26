e3a1d9ac617e7cffa27bc7c99bd6e7408be7c099
seonghyeonseon <leaf1993@naver.com>
2021-02-27 00:42:10 +0900

    Hash: Question02

--- 1. Hash/Question02.py
+++ 1. Hash/Question02.py
@@ -1,3 +1,5 @@
+# 문제를 잘못 이해한 풀이
+'''
 def solution(phone_book):
     cnt = len(phone_book)
     for i in range(cnt):
@@ -16,3 +18,58 @@ if __name__ == "__main__":
         print("True")
     else:
         print("False")
+'''
+
+# 답은 나오지만 해시를 사용하지 않음
+'''
+from collections import deque
+
+
+def solution(phone_book):
+    dq = deque(phone_book)
+    cnt = len(phone_book)
+    for _ in range(cnt):
+        a = dq.popleft()
+        for x in dq:
+            if a in x:
+                return False
+        else:
+            dq.append(a)
+    else:
+        return True
+
+
+if __name__ == "__main__":
+    # phone_book = ["119", "97674223", "1195524421"]
+    # phone_book = ["123", "456", "789"]
+    # phone_book = ["12", "123", "1235", "567", "88"]
+    phone_book = ["113", "44", "4544"]
+    if solution(phone_book):
+        print("True")
+    else:
+        print("False")
+'''
+
+
+# 풀이
+def solution(phone_book):
+    answer = True
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                return False
+    return answer
+
+if __name__ == "__main__":
+    # phone_book = ["119", "97674223", "1195524421"]
+    # phone_book = ["123", "456", "789"]
+    # phone_book = ["12", "123", "1235", "567", "88"]
+    phone_book = ["113", "44", "4544"]
+    if solution(phone_book):
+        print("True")
+    else:
